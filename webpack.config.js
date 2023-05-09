486f0b07b102d2de1d04b9e44ee2082189597195
Anthony Elison <tonyelison37@gmail.com>
2023-05-08 18:03:58 -0600

    Add initial css file and include an entry point for it in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 const path = require('path');
 
+const ROOT = '.';
+const SRC = 'src';
+const DIST = 'dist';
+const ROOT_SRC = `${ROOT}/${SRC}`;
+
 module.exports = {
-  entry: './src/index.js',
+  entry: [
+    `${ROOT_SRC}/index.js`,
+    `${ROOT_SRC}/style.css`
+  ],
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, DIST),
   },
   module: {
     rules: [
