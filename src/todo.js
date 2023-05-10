cdc3b3e886956bb3b197d47eb0be17e6e2cb1402
Anthony Elison <tonyelison37@gmail.com>
2023-05-09 18:51:46 -0600

    Add todo priority and status mappings

--- src/todo.js
+++ src/todo.js
@@ -1,10 +1,22 @@
+const PRIORITY = {
+  none: 0,
+  low: 1,
+  medium: 2,
+  high: 3,
+};
+
+const STATUS = {
+  incomplete: 0,
+  complete: 1,
+};
+
 const todoFactory = (id, title, description, dueDate, priority, status) => ({
   id,
   title,
   description,
   dueDate,
-  priority,
-  status,
+  priority: priority || PRIORITY.none,
+  status: status || STATUS.incomplete,
 });
 
 export default todoFactory;
