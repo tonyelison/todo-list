28ceabf7c0663101368cd0d6935c0ff477679b05
Anthony Elison <tonyelison37@gmail.com>
2023-05-09 18:54:15 -0600

    Privatize project todos array

--- src/project.js
+++ src/project.js
@@ -1,7 +1,7 @@
 const projectFactory = (id, title, description, initialTodos) => {
   let todos = initialTodos || [];
 
-  const todoList = () => todos;
+  const todoList = () => [...todos];
   const addTodo = (todo) => todos.push(todo);
   const removeTodo = (todoId) => {
     todos = todos.filter((todo) => todo.id !== todoId);
