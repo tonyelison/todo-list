c9e38c1ab8208cf36949e168c3df56434646a295
Anthony Elison <tonyelison37@gmail.com>
2023-05-09 18:43:46 -0600

    Add project factory function, test adding and removing todos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+import projectFactory from './project';
 import todoFactory from './todo';
 
 const todo = todoFactory(1, 'Title', 'Description', new Date(), 1, 0);
 console.log(todo);
+
+const project = projectFactory(1, 'Project Title', 'This is a project');
+console.log(project.todoList().length);
+
+project.addTodo(todo);
+console.log(project.todoList().length);
+
+project.removeTodo(todo.id);
+console.log(project.todoList().length);
---
+++ src/project.js
@@ -0,0 +1,20 @@
+const projectFactory = (id, title, description, initialTodos) => {
+  let todos = initialTodos || [];
+
+  const todoList = () => todos;
+  const addTodo = (todo) => todos.push(todo);
+  const removeTodo = (todoId) => {
+    todos = todos.filter((todo) => todo.id !== todoId);
+  };
+
+  return {
+    id,
+    title,
+    description,
+    todoList,
+    addTodo,
+    removeTodo,
+  };
+};
+
+export default projectFactory;
